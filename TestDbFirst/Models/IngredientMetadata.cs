1e9a850a663acc9239f5a06a76b696b09d9a79b5
David <david@techtree.eu>
2017-06-29 17:11:35 +0200

    Ingredients admin + Warehouse, Customer egységesítése

---
+++ TestDbFirst/Models/IngredientMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestDbFirst
+{
+    public class IngredientMetadata
+    {
+        [Display(Name = "Név")]
+        [Required(ErrorMessage = "Név megadása kötelező!")]
+        public string Name { get; set; }
+        [Display(Name = "Aktív")]
+        public bool IsActive { get; set; }
+    }
+
+    [MetadataType(typeof(IngredientMetadata))]
+    public partial class Ingredient
+    {
+    }
+}
