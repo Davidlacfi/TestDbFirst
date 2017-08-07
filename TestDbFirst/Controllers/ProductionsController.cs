a31c57899fe565b5f0f0e997e7592ab28dceb93d
David <david@techtree.eu>
2017-08-07 23:35:24 +0200

    Csak összetevővel rendelkező recept jelenik meg a gyártások listájában

--- TestDbFirst/Controllers/ProductionsController.cs
+++ TestDbFirst/Controllers/ProductionsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Validation;
 using System.Linq;
@@ -39,7 +40,16 @@ namespace TestDbFirst.Controllers
         public ActionResult Create()
         {
             ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name");
-            ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name");
+
+            var existingRecipe = new List<Recipe>();
+            foreach (var ai in db.Recipes)
+            {
+                if (db.RecipeIngredients.Any(x => x.Recipe_Id == ai.Id))
+                {
+                    existingRecipe.Add(ai);
+                };
+            }
+            ViewBag.Recipe_Id = new SelectList(existingRecipe, "Id", "Name");
             ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email");
             ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email");
             ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name");
@@ -147,87 +157,95 @@ namespace TestDbFirst.Controllers
             }
 
             ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name", production.Customer_Id);
-            ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name", production.Recipe_Id);
+            var existingRecipe = new List<Recipe>();
+            foreach (var ai in db.Recipes)
+            {
+                if (db.RecipeIngredients.Any(x => x.Recipe_Id == ai.Id))
+                {
+                    existingRecipe.Add(ai);
+                };
+            }
+            ViewBag.Recipe_Id = new SelectList(existingRecipe, "Id", "Name");
             ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email", production.CreatedBy);
             ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email", production.ChangedBy);
             ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name", production.Destination_Warehouse_Id);
             return View(production);
         }
 
-        // GET: Productions/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Production production = db.Productions.Find(id);
-            if (production == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name", production.Customer_Id);
-            ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name", production.Recipe_Id);
-            ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email", production.CreatedBy);
-            ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email", production.ChangedBy);
-            ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name", production.Destination_Warehouse_Id);
-            return View(production);
-        }
+        //// GET: Productions/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Production production = db.Productions.Find(id);
+        //    if (production == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name", production.Customer_Id);
+        //    ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name", production.Recipe_Id);
+        //    ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email", production.CreatedBy);
+        //    ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email", production.ChangedBy);
+        //    ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name", production.Destination_Warehouse_Id);
+        //    return View(production);
+        //}
 
-        // POST: Productions/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Recipe_Id,Customer_Id,Destination_Warehouse_Id,ProductionDate,Quantity,Remark,IsActive,CreatedBy,CreatedDate,ChangedBy,ChangedDate")] Production production)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(production).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name", production.Customer_Id);
-            ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name", production.Recipe_Id);
-            ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email", production.CreatedBy);
-            ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email", production.ChangedBy);
-            ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name", production.Destination_Warehouse_Id);
-            return View(production);
-        }
+        //// POST: Productions/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Recipe_Id,Customer_Id,Destination_Warehouse_Id,ProductionDate,Quantity,Remark,IsActive,CreatedBy,CreatedDate,ChangedBy,ChangedDate")] Production production)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(production).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.Customer_Id = new SelectList(db.Customers, "Id", "Name", production.Customer_Id);
+        //    ViewBag.Recipe_Id = new SelectList(db.Recipes, "Id", "Name", production.Recipe_Id);
+        //    ViewBag.CreatedBy = new SelectList(db.SystemUsers, "Id", "Email", production.CreatedBy);
+        //    ViewBag.ChangedBy = new SelectList(db.SystemUsers, "Id", "Email", production.ChangedBy);
+        //    ViewBag.Destination_Warehouse_Id = new SelectList(db.Warehouses, "Id", "Name", production.Destination_Warehouse_Id);
+        //    return View(production);
+        //}
 
-        // GET: Productions/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Production production = db.Productions.Find(id);
-            if (production == null)
-            {
-                return HttpNotFound();
-            }
-            return View(production);
-        }
+        //// GET: Productions/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Production production = db.Productions.Find(id);
+        //    if (production == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(production);
+        //}
 
-        // POST: Productions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Production production = db.Productions.Find(id);
-            db.Productions.Remove(production);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Productions/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Production production = db.Productions.Find(id);
+        //    db.Productions.Remove(production);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
